6121931e8f85770d50c6515599a38e7fd1cd3cd2
antonydoyle <antony@siege21.com>
2012-06-04 23:36:32 +0100

    Added additional repo facility

--- helper.php
+++ helper.php
@@ -10,9 +10,30 @@ defined('_JEXEC') or die('Direct Access to this location is not allowed.');
  
 $reponame = $params->get('reponame');
 $repopath = $params->get('repopath');
+$txtAbove = $params->get('textabove');
+
+$reponame1 = $params->get('reponame1');
+$repopath1 = $params->get('repopath1');
+$txtAbove1 = $params->get('textabove1');
+
+$reponame2 = $params->get('reponame2');
+$repopath2 = $params->get('repopath2');
+$txtAbove2 = $params->get('textabove2');
+
+$reponame3 = $params->get('reponame3');
+$repopath3 = $params->get('repopath3');
+$txtAbove3 = $params->get('textabove3');
+
+$reponame4 = $params->get('reponame4');
+$repopath4 = $params->get('repopath4');
+$txtAbove4 = $params->get('textabove4');
+
+$reponame5 = $params->get('reponame5');
+$repopath5 = $params->get('repopath5');
+$txtAbove5 = $params->get('textabove5');
+
 $jQuery = $params->get('jQuery'); 
 $gitCss = $params->get('gitcss'); 
-$txtAbove = $params->get('textabove');
  // Returns a reference to the global document object
 $doc 					= JFactory::getDocument();
  
--- tmpl/default.php
+++ tmpl/default.php
@@ -13,10 +13,71 @@
 <div id="ghtexttop"><p><?php echo $txtAbove ?></p></div>
 
 <div id="<?php echo $reponame ?>" class="gh_mod loading">&hellip;</div>
-
-
 <script>
 var reponame = "<?php echo $reponame; ?>";
 var repopath = "<?php echo $repopath; ?>";
+var reponame1 = "<?php echo $reponame1; ?>";
+var repopath1 = "<?php echo $repopath1; ?>";
+var reponame2 = "<?php echo $reponame2; ?>";
+var repopath2 = "<?php echo $repopath2; ?>";
+var reponame3 = "<?php echo $reponame3; ?>";
+var repopath3 = "<?php echo $repopath3; ?>";
+var reponame4 = "<?php echo $reponame4; ?>";
+var repopath4 = "<?php echo $repopath4; ?>";
+var reponame5 = "<?php echo $reponame5; ?>";
+var repopath5 = "<?php echo $repopath5; ?>";
+
 GHmod.init(reponame, repopath);
 </script>
+
+<?php 
+if ($reponame1 != NULL) {
+?>
+<script>
+GHmod.init(reponame1, repopath1)
+</script>
+<div id="ghtexttop"><p><?php echo $txtAbove1 ?></p></div>
+<div id="<?php echo $reponame1 ?>" class="gh_mod loading">&hellip;</div>
+<? } ?>
+
+<?php 
+if ($reponame2 != NULL) {
+?>
+<script>
+GHmod.init(reponame2, repopath2)
+</script>
+<div id="ghtexttop"><p><?php echo $txtAbove2 ?></p></div>
+<div id="<?php echo $reponame2 ?>" class="gh_mod loading">&hellip;</div>
+<? } ?>
+
+<?php 
+if ($reponame3 != NULL) {
+?>
+<script>
+GHmod.init(reponame3, repopath3)
+</script>
+<div id="ghtexttop"><p><?php echo $txtAbove3 ?></p></div>
+<div id="<?php echo $reponame3 ?>" class="gh_mod loading">&hellip;</div>
+<? } ?>
+
+<?php 
+if ($reponame4 != NULL) {
+?>
+<script>
+GHmod.init(reponame4, repopath4)
+</script>
+<div id="ghtexttop"><p><?php echo $txtAbove4 ?></p></div>
+<div id="<?php echo $reponame4 ?>" class="gh_mod loading">&hellip;</div>
+<? } ?>
+
+<?php 
+if ($reponame5 != NULL) {
+?>
+<script>
+GHmod.init(reponame5, repopath5)
+</script>
+<div id="ghtexttop"><p><?php echo $txtAbove5 ?></p></div>
+<div id="<?php echo $reponame5 ?>" class="gh_mod loading">&hellip;</div>
+<? } ?>
+
+
